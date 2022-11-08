5623cda60e93a246f97cef85b2901318fdbe4a07
Iliyan <iliyyaniordanov@gmail.com>
2022-11-08 16:05:20 +0200

    Adding Swagger documentation.

---
+++ src/main/java/com/springboot/blog/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.springboot.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Blog Rest Api",
+                "Blog documentation",
+                "1",
+                "Terms of service",
+                new Contact("Iliyan", "www.url.com", "iliyaniordanov@gmail.com"),
+                "License of API",
+                "API license url",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
